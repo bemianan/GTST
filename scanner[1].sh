148eb1acc1ee895873a9e59ee56dd7a4b2c878cd
Anan Gosa <anangosa112@gmail.com>
2026-06-07 20:22:24 +0300

    nse scan

--- scanner[1].sh
+++ scanner[1].sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check number of arguments
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <IP_ADDRESS> <NSE_SCRIPT>"
     echo "Example: $0 192.168.1.1 vuln"
@@ -9,6 +8,5 @@ fi
 
 TARGET=$1
 SCRIPT=$2
-
 echo "Scanning..."
 nmap  --script "$SCRIPT" "$TARGET"
